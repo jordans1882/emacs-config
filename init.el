4e8b90b5b519e9e615ed125ac0d9ec15744ff2c7
jordans1882 <jordans1882@gmail.com>
2020-09-24 11:35:15 -0600

    Sync up to date

--- init.el
+++ init.el
@@ -16,8 +16,12 @@
 (require 'pallet)
 (pallet-mode t)
 
+(defvar treemacs-no-load-time-warnings t)
+;; (set-face-attribute 'hl-line nil :background "#2d2d31")
 ;; (eval-when-compile (defvar misenplace-wm))
 ;; use-package to load and configure packages
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 (require 'use-package)
 (use-package misenplace
   :init
@@ -26,12 +30,20 @@
   :functions setup-misenplacewm
   :config
   ;;(setup-misenplacewm)
-  (setup-misenplaceterm)
+  ;;(setup-misenplaceterm)
   )
 
-  (set-default-font "DejaVu Sans Mono-30")
-  (set-face-attribute 'default nil :font "DejaVu Sans Mono-30" )
-  (set-frame-font "DejaVu Sans Mono-30" nil t)
+  (setq default-frame-alist '((font ."DejaVu Sans Mono-20")))
+
+  (setq backup-directory-alist `(("." . "~/.saves")))
+  ;; (set-default-font "DejaVu Sans Mono-50")
+  ;; (set-face-attribute 'default nil :font "DejaVu Sans Mono-50" )
+  ;; (set-frame-font "DejaVu Sans Mono-50" nil t)
+  ;;(setq default-frame-alist '((font . "DejaVu Sans Mono-50"))`
+
+
+(use-package haskell-mode)
+;; (treemacs-load-theme "Default")
 
 ;; set transparency
 (set-frame-parameter (selected-frame) 'alpha '(95 50))
@@ -41,6 +53,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/current_todo.org")))
  '(package-selected-packages
    (quote
     (yasnippet-snippets which-key web-mode undercover treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil smex smartparens rg prodigy powerline popwin poly-R pallet ox-reveal org-ref org-projectile org-pomodoro org-bullets nyan-mode multiple-cursors misenplace lorem-ipsum ivy-prescient ivy-posframe idle-highlight-mode flycheck-cask eyebrowse exwm-x expand-region exec-path-from-shell evil-nerd-commenter ess ert-runner ert-async elscreen dumb-jump drag-stuff doom-modeline dashboard counsel-projectile counsel-org-clock auto-complete ace-jump-mode))))
